32f2ea91d0c74b8417bef8c7c70fef4fd59d3a28
Prasan Roy <prasan@scleradb.com>
2020-02-01 23:26:35 +0530

    Corrected Source handling
    Ensure that the Source object is always closed after reading, success or failure.

--- src/main/scala/source/TextFilesResult.scala
+++ src/main/scala/source/TextFilesResult.scala
@@ -38,8 +38,7 @@ class TextFilesResult(
     /** Reads the text files and emits the data as an iterator on rows */
     override def rows: Iterator[ScalTableRow] = filesIter.map { f =>
         val src: Source = Source.fromFile(f)
-        val text: String = src.getLines().mkString("\n")
-        src.close()
+        val text: String = try src.getLines().mkString("\n") finally src.close()
 
         ScalTableRow(
             "FILE" -> CharConst(f.getCanonicalPath()),
